8e4186d9d46013a93ded820cc10b09eceddc5d50
Deep Detroja <deepdetroja1223@gmail.com>
2026-06-25 23:03:38 +0530

    set-up

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import { Dangrek } from "next/font/google";
+
+export default function Home() {
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
+}
